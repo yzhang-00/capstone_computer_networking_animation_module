6a235a78b5b906a1a92e274fff9b7efb4ca2e91e
yzhang-00 <yzhang7476@gmail.com>
2023-10-02 11:01:19 -0400

    feat: add main_gif draft (replace tmp.gif)

--- src/components/gif/Gif_main.tsx
+++ src/components/gif/Gif_main.tsx
@@ -1,16 +1,12 @@
 import wwwlogo from "../../assets/img/wwwlogo.gif";
-import tmp_gif from "../../assets/img/tmp.gif";
+import main_gif from "../../assets/img/main_gif_webbrowsing.gif";
 
 function Gif_main() {
   return (
     <div>
-      <div className="gif-logo-container">
-        <img src={wwwlogo} alt="wwwlogo" className="gif-logo" />
-      </div>
-
-      <div className="gif-container">
-        <img src={tmp_gif} alt="Overview of web-browsing" className="gif" />
-      </div>
+      <img className="gif-logo-container" src={wwwlogo} alt="wwwlogo" />
+      <img className="gif-container" src={main_gif} alt="Overview of web-browsing" />
+      
     </div>
   );
 }
