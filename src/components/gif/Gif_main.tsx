49b602a8359c8658b91e83a4c5b069e6a858a2c1
yaqi zhang <yaqizhang@yaqis-MacBook-Air-2.local>
2023-11-24 20:43:58 -0500

    adjust the logo position

--- src/components/gif/Gif_main.tsx
+++ src/components/gif/Gif_main.tsx
@@ -4,9 +4,12 @@ import main_gif from "../../assets/img/main_gif_webbrowsing.gif";
 function Gif_main() {
   return (
     <div>
-      <img className="gif-logo-container" src={wwwlogo} alt="wwwlogo" />
-      <img className="gif-container" src={main_gif} alt="Overview of web-browsing" />
-      
+      {/* <img className="gif-logo-container" src={wwwlogo} alt="wwwlogo" /> */}
+      <img
+        className="gif-container"
+        src={main_gif}
+        alt="Overview of web-browsing"
+      />
     </div>
   );
 }
