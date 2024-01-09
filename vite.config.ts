702892baf73be713e128faa79f85b83ee9984bac
Yixin Zhang <yzhang7476@gmail.com>
2024-01-08 19:59:50 -0500

    fix: assets refrence

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/capstone_computer_networking_animation_module/'
 })
